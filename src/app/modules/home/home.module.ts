3152a1a1046aa1d5fb410f504173bcf126560982
Kirill Voytov <utulalop@gmail.com>
2023-11-29 13:06:18 +0300

    stor-6/block-stock

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { HomeRoutes } from './home.routes';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutes],
+  imports: [CommonModule, HomeRoutes, NgOptimizedImage],
 })
 export class HomeModule {}
